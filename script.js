e1d461c5cf548430c99340766f822616d6d5b9cf
Jonathan Agatston <jagatston123@gmail.com>
2021-03-30 21:34:22 -0400

    made changes to the html and js

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
-var button = document.querySelector(".button");
-var inputValue = document.querySelector(".inputValue");
-var name = document.querySelector(".name");
-var desc = document.querySelector(".desc");
-var temp = document.querySelector(".temp");
+// Variables
+var searchForm = $("#search-form");
+var searchTermEl = $("#search-term");
+var imagesDisplay = $("#images-display");
 
-button.addEventListener('click',function(){
-    fetch(
-        "api.openweathermap.org/data/2.5/weather?q=" +
-          inputValue.value +
-          "&appid=50fd6bbfeac12f2c482fff81df793012"
-      )
-          .then(response => response.json())
-          .then(data => console.log(data))
-      
-      .catch(err => alert("Wrong city name!"))    
+// function to with api key
+searchForm.on("submit", function (event) {
+  event.preventDefault();
+
+  var searchTerm = searchTermEl.val();
+  console.log(searchTerm);
+  var apiKey = "50fd6bbfeac12f2c482fff81df793012";
+
+  var queryURL =
+    "http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid={50fd6bbfeac12f2c482fff81df793012}" +
+    apiKey +
+    "&q=" +
+    searchTerm;
+  
+  fetch(queryURL)
+    .then(function (response){
+        return response.json();
+    })
+    .then(function (data){
+        console.log(data);
+    });
+  
 });
+
